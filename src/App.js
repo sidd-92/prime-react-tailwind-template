037ecfae83dee1e39830893871e5c0d4f7c35bcc
Siddharth Natrajan <sidddharthvn2014@gmail.com>
2020-12-03 17:54:27 +0530

    Voucher Screen

--- src/App.js
+++ src/App.js
@@ -5,10 +5,12 @@ import { InputText } from "primereact/inputtext";
 import { InputTextarea } from "primereact/inputtextarea";
 import { Panel } from "primereact/panel";
 import { Button } from "primereact/button";
+import { vouchers } from "./services/dataservice";
 import { VoucherTable } from "./components/VoucherTable/VoucherTable";
 import { Dropdown } from "primereact/dropdown";
-
-import { Sidebar } from "primereact/sidebar";
+import { FileUpload } from "primereact/fileupload";
+import { SelectButton } from "primereact/selectbutton";
+import { Dialog } from "primereact/dialog";
 const items = [
   {
     label: "Vouchers",
@@ -25,6 +27,16 @@ const citySelectItems = [
   { label: "Paris", value: "PRS" },
 ];
 
+const voucherTypes = [
+  { label: "1", value: "1" },
+  { label: "2", value: "2" },
+  { label: "3", value: "3" },
+  { label: "4", value: "4" },
+];
+const activateOptions = [
+  { label: "Active", value: "Active" },
+  { label: "Not Active", value: "Not Active" },
+];
 const items_breadcrumb = [
   {
     label: "Vouchers",
@@ -36,24 +48,49 @@ const home = { icon: "pi pi-home" };
 class App extends React.Component {
   constructor(props) {
     super(props);
+    this.handleSelect = this.handleSelect.bind(this);
     this.state = {
       visible: false,
+      file: null,
+      voucherType: "",
     };
+  }
+  handleSelect(e) {
+    var files = e.files;
+    var file = files[0];
+    this.setState({ file: file });
+    console.log("Upload Handler Files", file);
   }
 
   render() {
+    console.log("VOUCHERS", vouchers);
     return (
       <React.Fragment>
         <Menubar start={() => <div className="font-bold text-2xl">Voucher Screen</div>} model={items} />
         <BreadCrumb model={items_breadcrumb} home={home} />
-        <Sidebar visible={this.state.visible} showCloseIcon={false} fullScreen onHide={() => this.setState({ visible: false })}>
+        <Dialog blockScroll maximized={true} header="Create New Voucher" className="w-4/5" visible={this.state.visible} onHide={() => this.setState({ visible: false })}>
           <div>
             <div className="font-bold text-3xl text-center">Create A New Voucher</div>
 
             <div className="mt-4">
               <div className="mb-4">
-                <Panel header="Voucher Names">
+                <Panel header="Voucher Basic">
                   <div className="p-fluid">
+                    <div className="p-field p-grid">
+                      <label htmlFor="firstname4" className="p-col-12 p-md-2">
+                        Voucher Type
+                      </label>
+                      <div className="p-col-12 p-md-10">
+                        <Dropdown
+                          value={this.state.voucherType}
+                          options={voucherTypes}
+                          onChange={(e) => {
+                            this.setState({ voucherType: e.value });
+                          }}
+                          placeholder="Select a Voucher Type"
+                        />
+                      </div>
+                    </div>
                     <div className="p-field p-grid">
                       <label htmlFor="firstname4" className="p-col-12 p-md-2">
                         Voucher Name (Internal)
@@ -96,7 +133,7 @@ class App extends React.Component {
                       <label htmlFor="lastname4" className="p-col-12 p-md-2">
                         Voucher Category
                       </label>
-                      <div className="p-col-12 p-md-6">
+                      <div className="p-col-12 p-lg-6">
                         <Dropdown
                           value={this.state.city}
                           options={citySelectItems}
@@ -106,20 +143,27 @@ class App extends React.Component {
                           placeholder="Select a City"
                         />
                       </div>
-                      <div className="p-col-12 p-md-4">
+                      <div className="p-col-12 p-lg-4 mt-2 sm:mt-0">
                         <div className="p-inputgroup">
                           <InputText placeholder="Add A New Category" />
                           <Button label="Add New Category" />
                         </div>
                       </div>
                     </div>
-
                     <div className="p-field p-grid">
                       <label htmlFor="lastname4" className="p-col-12 p-md-2">
-                        Voucher Name (Voucher Display)
+                        Voucher Is
+                      </label>
+                      <div className="p-col-12 p-md-2">
+                        <SelectButton value={this.state.active} options={activateOptions} onChange={(e) => this.setState({ active: e.value })}></SelectButton>
+                      </div>
+                    </div>
+                    <div className="p-field p-grid">
+                      <label htmlFor="lastname4" className="p-col-12 p-md-2">
+                        Voucher Image
                       </label>
                       <div className="p-col-12 p-md-10">
-                        <InputText id="lastname4" type="text" />
+                        <FileUpload name="demo" auto={false} mode="basic" ref={(el) => (this.upldRef = el)} customUpload={true} onSelect={(e) => this.handleSelect(e)} />
                       </div>
                     </div>
                   </div>
@@ -127,16 +171,24 @@ class App extends React.Component {
               </div>
             </div>
             <Button onClick={(e) => this.setState({ visible: false })} className=" bg-gray-500" label="Back" icon="pi pi-arrow-left" />
+            <Button
+              onClick={(e) => {
+                console.log("STATE", this.state.file);
+              }}
+              className=" bg-buttonGradient ml-3"
+              label="Create New Voucher"
+              icon="pi pi-plus"
+            />
           </div>
-        </Sidebar>
+        </Dialog>
 
         <div className="p-2 mt-4">
           <div className="flex items-center mb-4">
             <div>
-              <Button onClick={(e) => this.setState({ visible: true })} className="w-56 bg-buttonGradient" label="Create A New Voucher" icon="pi pi-check" />
+              <Button onClick={(e) => this.setState({ visible: true })} className="w-56 bg-buttonGradient" label="Create A New Voucher" icon="pi pi-plus" />
             </div>
           </div>
-          <VoucherTable />
+          <VoucherTable vouchers={vouchers} />
         </div>
       </React.Fragment>
     );
--- src/components/VoucherTable/VoucherTable.js
+++ src/components/VoucherTable/VoucherTable.js
@@ -6,66 +6,36 @@ import { Button } from "primereact/button";
 export class VoucherTable extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      globalFilter: null,
+    };
     this.renderAction = this.renderAction.bind(this);
     this.statusBodyTemplate = this.statusBodyTemplate.bind(this);
-    this.state = {
-      vouchers: [
-        {
-          id: 1,
-          name: "Discounted Apples1",
-          status: "Active",
-          type: 1,
-          creationDate: new Date().toDateString(),
-        },
-        {
-          id: 2,
-          name: "Discounted Apples2",
-          status: "Not Active",
-          type: 2,
-          creationDate: new Date().toDateString(),
-        },
-        {
-          id: 3,
-          name: "Discounted Apples3",
-          status: "Active",
-          type: 3,
-          creationDate: new Date().toDateString(),
-        },
-        {
-          id: 4,
-          name: "Discounted Apples14",
-          status: "Active",
-          type: 2,
-          creationDate: new Date().toDateString(),
-        },
-        {
-          id: 5,
-          name: "Discounted Apples22",
-          status: "Not Active",
-          type: 3,
-          creationDate: new Date().toDateString(),
-        },
-        {
-          id: 6,
-          name: "Discounted Apples32",
-          status: "Not Active",
-          type: 1,
-          creationDate: new Date().toDateString(),
-        },
-      ],
-    };
   }
   renderAction(rowData) {
     return (
-      <div className="flex items-center w-40">
-        <Button label={rowData.status === "Active" ? "De-Activate" : "Activate"} className={rowData.status === "Active" ? "bg-red-600 w-40" : "bg-green-500 w-40"} />
+      <div className="flex items-center w-full sm:w-40">
+        <Button
+          label={rowData.active === "Active" ? "De-Activate" : "Activate"}
+          className={rowData.active === "Active" ? "bg-red-600 w-full sm:w-40" : "bg-green-500 w-full sm:w-40"}
+        />
       </div>
     );
   }
 
   statusBodyTemplate(rowData) {
     return (
-      <div className={`p-1 w-24 text-center rounded-sm ${rowData.status.toLowerCase() === "active" ? "bg-green-500 text-white" : "bg-red-600 text-white"}`}>{rowData.status}</div>
+      <React.Fragment>
+        <div className={`p-1 w-24 text-center rounded-sm hidden sm:block ${rowData.active.toLowerCase() === "active" ? "bg-green-500 text-white" : "bg-red-600 text-white"}`}>
+          {rowData.active}
+        </div>
+        <div className="flex sm:hidden items-center">
+          <div className="font-bold mr-8">Status</div>
+          <div className={`p-1 w-24 text-center rounded-sm  ${rowData.active.toLowerCase() === "active" ? "bg-green-500 text-white" : "bg-red-600 text-white"}`}>
+            {rowData.active}
+          </div>
+        </div>
+      </React.Fragment>
     );
   }
 
@@ -83,18 +53,54 @@ export class VoucherTable extends React.Component {
       <div className="datatable-responsive-demo">
         <DataTable
           className="p-datatable-responsive-demo p-datatable-sm p-datatable-striped"
-          value={this.state.vouchers}
+          value={this.props.vouchers}
           paginator
           rows={100}
           globalFilter={this.state.globalFilter}
           header={header}
         >
-          <Column field="name" header="Name">
+          <Column
+            field="name_store"
+            header="Name"
+            body={(rowData) => (
+              <React.Fragment>
+                <div className="hidden sm:block">{rowData.name_store}</div>
+                <div className="flex sm:hidden">
+                  <div className="font-bold mr-8">Name</div>
+                  <div>{rowData.name}</div>
+                </div>
+              </React.Fragment>
+            )}
+          >
             Name
           </Column>
-          <Column field="type" header="Type"></Column>
-          <Column field="status" header="Status" body={this.statusBodyTemplate}></Column>
-          <Column field="creationDate" header="Date Created"></Column>
+          <Column
+            field="type"
+            header="Type"
+            body={(rowData) => (
+              <React.Fragment>
+                <div className="hidden sm:block">{rowData.type}</div>
+                <div className="flex sm:hidden bg-gray-200 py-1">
+                  <div className="font-bold mr-10">Type</div>
+                  <div>{rowData.type}</div>
+                </div>
+              </React.Fragment>
+            )}
+          ></Column>
+          <Column field="active" header="Status" body={this.statusBodyTemplate}></Column>
+          <Column
+            field="creation_date"
+            header="Date Created"
+            body={(rowData) => (
+              <React.Fragment>
+                <div className="hidden sm:block">{rowData.creation_date}</div>
+                <div className="flex sm:hidden bg-gray-200 py-1">
+                  <div className="font-bold mr-10">Date Created</div>
+                  <div>{rowData.creationDate}</div>
+                </div>
+              </React.Fragment>
+            )}
+          ></Column>
           <Column field="action" header="Action" body={(rowData) => this.renderAction(rowData)}></Column>
         </DataTable>
       </div>
